0ff55cdeabb080ad3b293e38a7631fd43503fb13
cafe1004 <cafe1004@gmail.com>
2020-09-18 16:19:45 +0900

    Edit readme.md

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+function Food({fav}) {
+  return (
+    <h3>I love {fav}</h3>
+  );
+}
+
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      <Food fav="김치" />
+      <Food fav="치킨" />
+      <Food fav="햄버거" />
+      <Food fav="피자" />
+      <Food fav="콜라" />
     </div>
     );
 }
